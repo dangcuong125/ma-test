f9b63abd303eb537b91123fd2b703163a6dcb892
Cuong Dang <dangcuong1225@gmail.com>
2023-06-26 14:17:49 +0700

    init project

--- roots.config.js
+++
@@ -1,9 +0,0 @@
-const path = require("path");
-
-module.exports = {
-  originDir: path.resolve(__dirname, "src/roots"),
-  localizedDir: path.resolve(__dirname, "src/app"),
-  locales: ["en", "vi"],
-  defaultLocale: "vi",
-  prefixDefaultLocale: true, // serves "en" locale on / instead of /en
-};
