81fd375f0b779a72c4649dfe573c1623c552569b
ndtien14 <ndtien.tesosoft@gmail.com>
2023-07-04 05:20:10 +0700

    upload user tab

--- src/app/[lang]/(noti)/noti-list/page.tsx
+++ src/app/[lang]/(noti)/noti-list/page.tsx
@@ -1,22 +1,11 @@
-import { getNotiList } from "../noti-common/service";
-import NextPageListNoti from "./components/NextPageListNoti";
-import NoDataNoti from "./components/NoDataNoti";
-import NotiHeader from "./components/NotiHeader";
-import NotiItem from "./components/NotiItem";
+import NotiList from ".";
+import LayoutAppWithUserTab from "../../(web)/LayoutWithUserTab";
 
-const NotiListPage = async () => {
-  const dataNoti = await getNotiList({ page: 1, limit: 10 });
-  const listNoti = dataNoti.items || [];
-  const isNotFound = !listNoti.length;
+const NotiListPage = () => {
   return (
-    <main>
-      <NotiHeader />
-      {listNoti.map((item) => (
-        <NotiItem key={item.id} notiItem={item} />
-      ))}
-      <NextPageListNoti />
-      <NoDataNoti isOpen={isNotFound} />
-    </main>
+    <LayoutAppWithUserTab>
+      <NotiList />
+    </LayoutAppWithUserTab>
   );
 };
 
---
+++ src/app/[lang]/(web)/layoutApp/components/side-bar/index.tsx
@@ -0,0 +1,51 @@
+"use client";
+import { Stack } from "@mui/material";
+import React from "react";
+import { SIDE_BAR_ITEM } from "./constants";
+import SideBarItem from "./components/SideBarItem";
+import { usePathname } from "next/navigation";
+import useTranslation from "next-translate/useTranslation";
+import Breadcrumbs from "@/common/components/Breadcrumbs";
+import SideBarHeader from "./components/SideBarHeader";
+
+const SideBarUser = () => {
+  const currentPath = usePathname();
+  const { t } = useTranslation("common");
+
+  const indexCurrentPage = SIDE_BAR_ITEM.findIndex((item) =>
+    currentPath.includes(item.path)
+  );
+
+  return (
+    <Stack>
+      <Breadcrumbs
+        links={[
+          {
+            name: `${t("sidebar.home")}`,
+            href: "/",
+          },
+          {
+            name: `${t(SIDE_BAR_ITEM[indexCurrentPage].name)}`,
+            href: SIDE_BAR_ITEM[indexCurrentPage].path,
+          },
+        ]}
+        activeLast
+      />
+      <SideBarHeader />
+      <Stack gap={"12px"} mt={"42px"}>
+        {SIDE_BAR_ITEM.map((item, index) => (
+          <SideBarItem
+            name={item.name}
+            src={item.src}
+            type={item.type}
+            key={index}
+            path={item.path}
+            isActiveItem={currentPath.includes(item.path)}
+          />
+        ))}
+      </Stack>
+    </Stack>
+  );
+};
+
+export default SideBarUser;
