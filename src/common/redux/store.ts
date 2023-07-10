cbee9307a1e5064c8fa7e2aa254a896819b9d622
bottle alimi <phammanhtb2001@gmail.com>
2023-07-10 10:14:49 +0700

    setup redux persist

--- src/common/redux/store.ts
+++ src/common/redux/store.ts
@@ -6,10 +6,22 @@ import {
   TypedUseSelectorHook,
 } from "react-redux";
 import { rootReducer } from "./rootReducer";
+import storage from 'redux-persist/lib/storage';
+import { persistReducer, persistStore } from "redux-persist";
+
+const persistConfig = {
+  key: 'root',
+  storage,
+  keyPrefix: 'ecomloyalty-',
+  whiteList: ['authLogin']
+}
+const persistedReducer = persistReducer(persistConfig, rootReducer);
 
 export const store = configureStore({
-  reducer: rootReducer,
+  reducer: persistedReducer,
 });
+
+export const persistor = persistStore(store);
 
 export type RootState = ReturnType<typeof store.getState>;
 
